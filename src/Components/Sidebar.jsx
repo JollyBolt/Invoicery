33d42c0acedfb6977f1abed439621b5f150d8a98
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-03-28 00:13:53 +0530

    Edited CSS

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -24,92 +24,90 @@ function Sidebar() {
 
   return (
     <div 
-    // className={`${[location.pathname === "/" ? "hidden" : ""]}`}
+    // className={`${[location.pathname==='/'?'hidden':'']}`}
     >
-      <Link
-        onClick={() => {
-          localStorage.removeItem("loggedIn");
-          navigate("/");
-        }}
-        to={"/"}
-        className="bg-white fixed right-5 top-7 outline outline-1 w-[70px] px-1 hover:text-white hover:bg-red-500 text-center outline-red-500 text-red-500 rounded-sm  "
-      >
-        Logout
-      </Link>
-      <nav className="top-0  fixed  z-10">
-        <div
-          className={`bg-[#0e0e0e] min-h-screen ${
-            open ? "w-72" : "w-16"
-          } duration-500 text-gray-100 px-4`}
+       <Link onClick={()=>{
+        localStorage.removeItem('loggedIn');
+         navigate('/')
+       }}
+              to={'/'}
+              className="bg-white fixed right-5 top-7 outline outline-1 w-[70px] px-1 hover:text-white hover:bg-red-500 text-center outline-red-500 text-red-500 rounded-sm  "
+            >
+              Logout
+            </Link>
+    <nav className="top-0  fixed  z-10">
+      <div
+        className={`bg-[#0e0e0e] min-h-screen ${
+          open ? "w-72" : "w-16"
+        } duration-500 text-gray-100 px-4`}
         >
-          <div className="py-3 flex justify-end">
-            <HiMenuAlt3
-              size={26}
-              className="cursor-pointer"
-              onClick={() => setOpen(!open)}
+        <div className="py-3 flex justify-end">
+          <HiMenuAlt3
+            size={26}
+            className="cursor-pointer"
+            onClick={() => setOpen(!open)}
             />
-          </div>
+        </div>
 
-          <div className="mt-4 flex flex-col gap-4 relative">
-            <div className="overflow-hidden text-nowrap">
-              <img
-                src={"/logo.png"}
-                width={40}
-                height={40}
-                alt="Logo"
-                className="inline"
+        <div className="mt-4 flex flex-col gap-4 relative">
+          <div className="overflow-hidden text-nowrap">
+            <img
+              src={"/logo.png"}
+              width={40}
+              height={40}
+              alt="Logo"
+              className="inline"
               />
+            <h2
+              style={{
+                transitionDelay: `${2}00ms`,
+              }}
+              className={`whitespace-pre duration-500 text-transparent font-semibold text-lg bg-clip-text bg-gradient-to-br from-indigo-800 to-teal-400 inline ${
+                !open && "opacity-0 translate-x-28 overflow-hidden"
+              }`}
+              >
+              Invoicery
+            </h2>
+          </div>
+          {menus?.map((menu, i) => (
+            <Link
+            to={menu?.link}
+            key={i}
+            className={` ${
+              menu?.margin && "mt-5"
+            } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+            >
+              <div>{React.createElement(menu?.icon, { size: "20" })}</div>
               <h2
                 style={{
-                  transitionDelay: `${2}00ms`,
+                  transitionDelay: `${i + 3}00ms`,
                 }}
-                className={`whitespace-pre duration-500 text-transparent font-semibold text-lg bg-clip-text bg-gradient-to-br from-indigo-800 to-teal-400 inline ${
+                className={`whitespace-pre duration-500 ${
                   !open && "opacity-0 translate-x-28 overflow-hidden"
                 }`}
-              >
-                 Invoicery 
+                >
+                {menu?.name}
               </h2>
-            </div>
-            {menus?.map((menu, i) => (
-              <Link
-              // style={{paddingRight:'75%'}}
-                to={menu?.link}
-                key={i}
-                className={` ${
-                  menu?.margin && "mt-5"
-                } group flex items-center text-sm  gap-x-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
-              >
-                <div className="mx-auto">{React.createElement(menu?.icon, { size: "20" })}</div>
-                <h2
-                  style={{
-                    transitionDelay: `${i + 3}00ms`,
-                  }}
-                  className={`whitespace-pre duration-500 ${
-                    !open && "opacity-0 translate-x-28 overflow-hidden"
-                  }`}
+              <h2
+                className={`${
+                  open && "hidden"
+                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
                 >
-                  {menu?.name}
-                </h2>
-                <h2
-                  className={`${
-                    open && "hidden"
-                  } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
-                >
-                  {menu?.name}
-                </h2>
-              </Link>
-            ))}
-            {/* {open ? (
+                {menu?.name}
+              </h2>
+            </Link>
+          ))}
+          {/* {open ? (
             
             ) : (
               <button type="button" className=" flex items-center justify-center bg-white text-red-500 w-[25px] outlin text-lg text-center mx-auto p-1 rounded-sm hover:text-white hover:bg-red-500 outline outline-1 outline-red-500">
               <IoPowerOutline />
               </button>
             )} */}
-          </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+            </div>
   );
 }
 

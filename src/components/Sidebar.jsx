8edcfa9eaf2e72c0305aa916a620939e359f6455
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-03-30 19:28:47 +0530

    Finished Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,8 @@ import { FaUser } from "react-icons/fa6";
 import { FaHandHoldingDollar } from "react-icons/fa6";
 import { FaFileInvoiceDollar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { IoPowerOutline } from "react-icons/io5";
+import data from "../demoData.json";
 
 function Sidebar({ open, setOpen, navBg, navHeading }) {
   const location = useLocation();
@@ -24,98 +26,119 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
     <div
     // className={`${[location.pathname==='/'?'hidden':'']}`}
     >
-      <Link
-        onClick={() => {
-          localStorage.removeItem("loggedIn");
-          navigate("/");
-        }}
-        to={"/"}
-        className="bg-white fixed right-5 top-7 outline outline-1 w-[70px] px-1 hover:text-white hover:bg-red-500 text-center outline-red-500 text-red-500 rounded-sm  "
-      >
-        Logout
-      </Link>
-
       <nav id="nav" className="top-0  fixed  z-10">
         <div
           style={{ backgroundColor: `var(--${navBg})` }}
-          className={` min-h-screen ${open ? "w-64" : "w-16"} duration-500 ${
+          className={`flex flex-col justify-between pb-5 min-h-screen ${
+            open ? "w-64" : "w-16"
+          } duration-500 ${
             open && "delay-[25ms]"
           } transition-all text-gray-100 px-4`}
         >
-          <div className={`py-3 flex pl-[5px]`}>
-            <HiMenuAlt2
-              size={24}
-              className="cursor-pointer"
-              onClick={() => setOpen(!open)}
-            />
-          </div>
-
-          <div className="mt-4 flex flex-col gap-4 relative">
-            <div className="overflow-hidden text-nowrap">
-              <img
-                src={"/logo.png"}
-                width={40}
-                height={40}
-                alt="Logo"
-                className="inline"
+          <div>
+            <div className={`py-3 flex pl-[5px]`}>
+              <HiMenuAlt2
+                size={24}
+                className="cursor-pointer"
+                onClick={() => setOpen(!open)}
               />
-              <h2
-                style={{
-                  transitionDelay: `${2}00ms`,
-                  color: `var(--${navHeading})`,
-                }}
-                className={`whitespace-pre duration-500 text-transparent font-semibold text-lg  inline ${
-                  !open && "opacity-0 translate-x-28 overflow-hidden"
-                }`}
-              >
-                Invoicery
-              </h2>
             </div>
-            {menus?.map((menu, i) => (
-         
 
-              <Link
-              // onMouseEnter={(e)=>{
-              //   e.target.style.backgroundColor=`var(--${linkHover})`
-              // }}
-              // onMouseLeave={(e)=>{
-              //   e.target.style.backgroundColor=`var(--${navBg})`
-              // }}
-              to={menu?.link}
-              key={i}
-              className={` ${
-                menu?.margin && "mt-5"
-              } group flex items-center text-sm  gap-3.5 font-medium py-2 pl-1 hover:bg-neutral-600/30  rounded-md`}
-              >
-                <div>{React.createElement(menu?.icon, { size: "24" })}</div>
+            <div className="mt-4 flex flex-col gap-4 justify-between relative">
+              <div className="overflow-hidden text-nowrap">
+                <img
+                  src={"/logo.png"}
+                  width={40}
+                  height={40}
+                  alt="Logo"
+                  className="inline"
+                />
                 <h2
                   style={{
-                    transitionDelay: `${i + 3}50ms`,
+                    transitionDelay: `${2}00ms`,
+                    color: `var(--${navHeading})`,
                   }}
-                  className={`whitespace-pre text-md duration-300 ${
-                    !open && "opacity-0 translate-x-24 overflow-hidden"
+                  className={`whitespace-pre duration-500 text-transparent font-semibold text-lg  inline ${
+                    !open && "opacity-0 translate-x-28 overflow-hidden"
                   }`}
-                  >
-                  {menu?.name}
+                >
+                  Invoicery
                 </h2>
+              </div>
+              {menus?.map((menu, i) => (
+                <Link
+                  to={menu?.link}
+                  key={i}
+                  className={` ${
+                    menu?.margin && "mt-5"
+                  } group flex items-center text-sm  gap-3.5 font-medium py-2 pl-1 hover:bg-neutral-600/30  rounded-md`}
+                >
+                  <div>{React.createElement(menu?.icon, { size: "24" })}</div>
+                  <h2
+                    style={{
+                      transitionDelay: `${i + 3}50ms`,
+                    }}
+                    className={`whitespace-pre text-md duration-300 ${
+                      !open && "opacity-0 translate-x-24 overflow-hidden"
+                    }`}
+                  >
+                    {menu?.name}
+                  </h2>
 
-                <h2
-                  className={`${
-                    open && "hidden"
-                  } absolute left-20 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+                  <h2
+                    className={`${
+                      open && "hidden"
+                    } absolute left-20 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
                   >
-                  {menu?.name}
-                </h2>
-              </Link>
-              
-            ))}
-            {/* {open ? (
-            
-            ) : (
-              <button type="button" className=" flex items-center justify-center bg-white text-red-500 w-[25px] outlin text-lg text-center mx-auto p-1 rounded-sm hover:text-white hover:bg-red-500 outline outline-1 outline-red-500">
-              <IoPowerOutline />
-              </button>
-            )} */}
+                    {menu?.name}
+                  </h2>
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-4 pt-5 border-t border-gray-500">
+            <Link
+              onClick={() => {
+                localStorage.removeItem("loggedIn");
+                navigate("/");
+              }}
+              to={"/"}
+              className={`bg-white  overflow-hidden flex flex-nowrap  py-1 px-1 hover:text-white hover:bg-red-500 ${
+                open ? "rounded-sm" : "rounded-full"
+              } transition-all duration-200 ease-linear text-red-500  `}
+            >
+              <IoPowerOutline
+                size={24}
+                className={`inline absolute rounded-full`}
+              />
+              <span
+                className={`inline-block mx-auto overflow-hidden  pl-7 ${
+                  !open && "opacity-0 transition-opacity"
+                }`}
+              >
+                Logout
+              </span>
+            </Link>
+            <div className="flex flex-row overflow-hidden">
+              <div className="absolute flex justify-center items-center py-[1px] w-8 bg-secondary text-black  mx-auto rounded-sm border-2 border-neutral-400 border-solid">
+                <div>
+                  {data.name.split(" ").map(function (word, i) {
+                    return (
+                      <span key={i} className="inline-block text-lg">
+                        {word[0]}
+                      </span>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-col flex-nowrap ml-14">
+                <h3 className="text-lg leading-none font-semibold text-white">
+                  {data.name}
+                </h3>
+                <p className="text-xs text-white">{data.email}</p>
+              </div>
+            </div>
           </div>
         </div>
       </nav>

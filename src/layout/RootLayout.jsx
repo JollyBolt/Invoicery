595b6b34dd71c61654370f2494693e85830330d9
Ishan <Braviaspan2002@gmail.com>
2024-04-02 01:35:06 +0530

    deleted commented code

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const RootLayout = () => {
   const [open, setOpen] = useState(false);
-  // const [navWidth, setNavWidth] = useState(open ? 210 : 64);
-  
-  // useEffect(() => {
-  //   setNavWidth(open ? 210 : 64);
-  //   document.getElementById("parent").style.width =
-  //     window.innerWidth - navWidth;
-  //   document.getElementById("parent").style.marginLeft = navWidth;
-  // }, [open]);
+
   return (
     <>
       <Sidebar open={open} setOpen={setOpen} navBg='primary' navHeading='secondary' />
-      <div id="parent" className={`transition-all pl-3 duration-500 ${open?`ml-[210px]`:`ml-[64px] `} bg-secondary`}
-        // style={{ width: document.innerWidth - navWidth, marginLeft: navWidth }}
+      <div id="parent" className={`transition-all duration-500 ${open?`ml-[210px]`:`ml-[64px] `} `}
       >
         <Outlet />
       </div>

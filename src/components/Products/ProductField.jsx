78bf0d138ab07009003046f6c1f15d3253be597d
Ishan <Braviaspan2002@gmail.com>
2024-05-26 12:54:06 +0530

    Created product field for product table

---
+++ src/components/Products/ProductField.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProductField = ({index,name,hsncode,price,currentPage}) => {
+    return (
+        <div
+            className={`h-14 overflow-hidden border-sate-400 border-b`}
+        >
+            <div
+                className={`w-full h-full flex flex-nowrap items-center justify-between py-2 px-5 border-x font-light`}
+            >
+                <h3 className=" capitalize w-1/4 font-light ">{index+1 + (currentPage-1)*10}</h3>
+                <h3 className=" capitalize w-1/4 font-light ">{name}</h3>
+                {/* </div> */}
+                <h4 className="  w-1/4">{hsncode}</h4>
+                <h4 className=" w-1/4">{price}</h4>
+            </div>
+        </div>
+    )
+}
+
+export default ProductField

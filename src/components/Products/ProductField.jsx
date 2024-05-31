bf833d5e9343a8ecb240870c9974aaf9029efa18
Ishan <Braviaspan2002@gmail.com>
2024-06-01 02:49:35 +0530

    Changed index

--- src/components/Products/ProductField.jsx
+++ src/components/Products/ProductField.jsx
@@ -8,9 +8,8 @@ const ProductField = ({index,name,hsncode,price,currentPage}) => {
             <div
                 className={`w-full h-full flex flex-nowrap items-center justify-between py-2 px-5 border-x font-light`}
             >
-                <h3 className=" capitalize w-1/4 font-light ">{index+1 + (currentPage-1)*10}</h3>
+                <h3 className=" capitalize w-1/4 font-light ">{index}</h3>
                 <h3 className=" capitalize w-1/4 font-light ">{name}</h3>
-                {/* </div> */}
                 <h4 className="  w-1/4">{hsncode}</h4>
                 <h4 className=" w-1/4">{price}</h4>
             </div>

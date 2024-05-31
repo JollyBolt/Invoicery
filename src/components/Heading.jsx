eac3efdb6ecdf57195e7667b601a42031d2fb1e8
Ishan <Braviaspan2002@gmail.com>
2024-06-01 02:44:37 +0530

    added breadcrumbs

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Breadcrumb from './Breadcrumb'
 
 const Heading = ({name}) => {
   return (
     <div>
         <p className='text-4xl font-black text-primary first-letter:text-5xl uppercase'>{name}</p>
+        <Breadcrumb />
     </div>
   )
 }

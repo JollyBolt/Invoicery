22cee2e5d6c6263088d27b9c14c81d7833c7c20d
Ishan <Braviaspan2002@gmail.com>
2024-06-12 23:01:48 +0530

    Created columns for table

---
+++ src/components/Table/Columns.jsx
@@ -0,0 +1,67 @@
+import CustomerActionsDropdown from "./CustomerActionsDropdown"
+import ProductActionsDropdown from "./ProductActionsDropdown"
+
+export const productColumns = [
+  {
+    id: "col1",
+    header: "Index",
+    cell: (row) => {
+      return row.row.index + 1
+    },
+  },
+  {
+    id: "col2",
+    header: "Name",
+    accessorKey: "name",
+  },
+  {
+    id: "col3",
+    header: "HSN Code",
+    accessorKey: "hsn_code",
+  },
+  {
+    id: "col4",
+    header: "Price(INR)",
+    accessorKey: "price",
+  },
+  {
+    id: "col5",
+    cell: (row) => <ProductActionsDropdown row={row.row}  />,
+  },
+]
+
+export const customerColumns = [
+    {
+        id:'col1',
+        header: 'Index',
+        cell: (row) => {return row.row.index + 1 }
+    },
+    {
+        id: 'col2',
+        header: 'Client',
+        accessorKey: 'client'
+    },
+    {
+        id: 'col3',
+        header: 'Phone',
+        accessorKey: 'phone'
+    },
+    {
+        id: 'col4',
+        header: 'State',
+        accessorKey: 'address.state'
+    },
+    {
+        id: 'col5',
+        header: 'City',
+        accessorKey: 'address.city'
+    },
+    {
+        id: 'col6',
+        cell: (row) => <CustomerActionsDropdown row={ row.row }  /> 
+    }
+]
+
+export const invoiceColumns = [
+
+]

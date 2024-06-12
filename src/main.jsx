27f7c13f6e71e72a04df8037027cd1d2d68a5261
Ishan <Braviaspan2002@gmail.com>
2024-06-12 14:59:24 +0530

    Added react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { BrowserRouter } from "react-router-dom"
+import { disableReactDevTools } from "@fvilers/disable-react-devtools"
+import { Provider } from "react-redux"
+import store from "./redux/store.js"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query"
 
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools();
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools()
 }
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <GoogleOAuthProvider clientId="760088389479-l0814dmqkq4hlacel14npfrpfd60hpg6.apps.googleusercontent.com">
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </GoogleOAuthProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-);
+)

33087be046805a3782179b2abb02d30b5622e63c
Ishan <Braviaspan2002@gmail.com>
2024-06-12 13:05:20 +0530

    Created customer layout

---
+++ src/layout/CustomerLayout.jsx
@@ -0,0 +1,29 @@
+import {useEffect,useState} from "react"
+
+import { Outlet } from "react-router-dom"
+import Auth from "../components/Auth"
+import CreateCustomer from "../components/Customer/CreateCustomer"
+import Heading from "../components/Heading"
+import { useDispatch, useSelector } from "react-redux"
+import { authSlice } from "../redux/slices/authSlice"
+import PageWrapper from "../hoc/PageWrapper"
+
+const CustomerLayout = () => {
+    const { refreshAuth } = authSlice.actions
+    const dispatch = useDispatch()
+    const { loggedIn } = useSelector((state) => state.auth)
+    useEffect(() => {
+      dispatch(refreshAuth())
+    }, [])
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className="flex min-h-[calc(100dvh-40px)] w-full flex-col">
+      <CreateCustomer open={open} setOpen={setOpen} />
+      <Heading name="Customers" />
+      <div className="mt-5">{loggedIn === false ? <Auth /> : <Outlet />}</div>
+    </div>
+  )
+}
+
+export default PageWrapper(CustomerLayout)

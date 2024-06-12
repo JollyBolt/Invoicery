9bc0234ea8941bd08e7d192069b0ce9aad1488c2
Ishan <Braviaspan2002@gmail.com>
2024-06-12 13:04:32 +0530

    Created table component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import {
   useReactTable,
   getCoreRowModel,
@@ -7,58 +7,55 @@ import {
 import { BsThreeDotsVertical } from "../../assets"
 
 const Table = ({ tableColumns, tableData }) => {
-    const [showActions, setShowActions] = useState(false)
-    const columns = useMemo(() => tableColumns, [])
-    const data = useMemo(() => tableData, [tableData])
-    // console.log(data)
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-    })
-    return (
-        <table className="w-full">
-            <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <tr key={headerGroup.id}>
-                        <th>Index</th>
-                        {headerGroup.headers.map((header) => (
-                            <th key={header.id}>
-                                {flexRender(
-                                    header.column.columnDef.header,
-                                    header.getContext(),
-                                )}
-                            </th>
-                        ))}
-                        <th>Actions</th>
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map((row,index) => (
-                    <tr key={row.id}>
-                        <td >{ index +1}</td>
-                        {
-                            row.getVisibleCells().map((cell) => (
-                                <td key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </td>
-                            ))
-                        }
-                        <td>
-                            <button>
-                                <BsThreeDotsVertical />
-                                <div className={`absolute ${!showActions && "hidden"} flex flex-col`}>
-                                    
-
-                                </div>
-                            </button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    )
+  const [showActions, setShowActions] = useState(false)
+  const columns = useMemo(() => tableColumns, [])
+  const data = useMemo(() => tableData, [tableData])
+  // console.log(data)
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+  return (
+    <table className="w-full">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            <th>Index</th>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+              </th>
+            ))}
+            <th>Actions</th>
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row, index) => (
+          <tr key={row.id}>
+            <td>{index + 1}</td>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+            <td>
+              <button>
+                <BsThreeDotsVertical />
+                <div
+                  className={`absolute ${!showActions && "hidden"} flex flex-col`}
+                ></div>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default Table

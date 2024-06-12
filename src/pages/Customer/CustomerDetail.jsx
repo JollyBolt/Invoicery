b40bc4d206d467fd178bfff99f29947bd1f46dfd
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-06-12 15:09:41 +0530

    Setup of customer detail page complete

--- src/pages/Customer/CustomerDetail.jsx
+++ src/pages/Customer/CustomerDetail.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchSingleCustomer } from "../../redux/slices/customerSlice"
 
 const CustomerDetail = () => {
-  return (
-    <div>CustomerDetail</div>
-  )
+  const id = useParams().id
+  // console.log(id)
+  const dispatch = useDispatch()
+  const { customers } = useSelector((state) => state.customers)
+  const customerDetails = customers[0]
+  useEffect(() => {
+    dispatch(fetchSingleCustomer(id))
+  }, [])
+  // console.log(customerDetails)
+  return <div>CustomerDetail</div>
 }
 
 export default CustomerDetail

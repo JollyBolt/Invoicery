74d4b9bde3365e1daba0a936600a2c7b3e21fa4e
Ishan <Braviaspan2002@gmail.com>
2024-06-12 15:09:20 +0530

    Uninstalled react query

--- src/main.jsx
+++ src/main.jsx
@@ -7,25 +7,22 @@ import { disableReactDevTools } from "@fvilers/disable-react-devtools"
 import { Provider } from "react-redux"
 import store from "./redux/store.js"
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query"
+
 
 if (process.env.NODE_ENV === "production") {
   disableReactDevTools()
 }
 
-const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <GoogleOAuthProvider clientId="760088389479-l0814dmqkq4hlacel14npfrpfd60hpg6.apps.googleusercontent.com">
-          <QueryClientProvider client={queryClient}>
+
             <App />
-          </QueryClientProvider>
+
         </GoogleOAuthProvider>
       </Provider>
     </BrowserRouter>

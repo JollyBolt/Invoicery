021c19b2f29dc80941400d2b8f28149f79ab527a
Ishan <Braviaspan2002@gmail.com>
2024-06-17 15:48:01 +0530

    Checking state

--- src/components/Invoice/InvoicePreview.jsx
+++ src/components/Invoice/InvoicePreview.jsx
@@ -1,9 +1,11 @@
 import React, { forwardRef } from 'react'
 
 const InvoicePreview = forwardRef((props, ref) => {
+  const { invoiceState } = props
+  console.log(invoiceState)
     return (
       <div ref={ref} className='bg-white min-h-full  p-2 '>
-        InvoicePreview
+        {/* {invoiceState} */}
         <div className='w-1/2 bg-red-500 h-[80vh]'></div>
         {/* <div className='w-1/2 bg-blue-500 h-[80vh]'></div> */}
 

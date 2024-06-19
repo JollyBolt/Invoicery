7ecdae8974921521d63d89b3d0c9952ae0833cb9
Ishan <Braviaspan2002@gmail.com>
2024-06-20 01:44:28 +0530

    Continued with invoice preview

--- src/components/Invoice/InvoicePreview.jsx
+++ src/components/Invoice/InvoicePreview.jsx
@@ -1,16 +1,30 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef,useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux"
 
 const InvoicePreview = forwardRef((props, ref) => {
+  const dispatch = useDispatch()
+  const { loggedIn } = useSelector((state) => state.auth)
+  const { user, loading } = useSelector((state) => state.user)
+
+  useEffect(() => {
+    async function getUserData() {
+      if (loggedIn) {
+        await dispatch(getProfile())
+      }
+    }
+    getUserData()
+  }, [loggedIn])
   const { invoiceState } = props
+  const { invoiceNumber } = invoiceState
   console.log(invoiceState)
     return (
-      <div ref={ref} className='bg-white min-h-full  p-2 '>
+      <div ref={ref} className="min-h-full bg-white p-2">
+        {invoiceNumber}
         {/* {invoiceState} */}
-        <div className='w-1/2 bg-red-500 h-[80vh]'></div>
+        {/* <div className="h-[80vh] w-1/2 bg-red-500"></div> */}
         {/* <div className='w-1/2 bg-blue-500 h-[80vh]'></div> */}
-
       </div>
-  )
+    )
 })
 
 

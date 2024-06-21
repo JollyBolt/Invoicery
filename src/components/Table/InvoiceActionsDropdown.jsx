38e438bfb18c9f66aa45102e338be1a1de831745
Ishan <Braviaspan2002@gmail.com>
2024-06-22 01:47:28 +0530

    created invoice actions dropdown

--- src/components/Table/InvoiceActionsDropdown.jsx
+++ src/components/Table/InvoiceActionsDropdown.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from "react"
+import { BsThreeDotsVertical } from "../../assets"
 
 const InvoiceActionsDropdown = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <div>InvoiceActionsDropdown</div>
+    <div>
+      <div
+        className="cursor-pointer overflow-hidden rounded-rounded"
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        <BsThreeDotsVertical />
+        <div
+          className={`absolute ${!open && "scale-0"} flex w-[80px] origin-top-left flex-col bg-white transition-all`}
+        >
+          <button
+            className="border p-2 hover:bg-gray-50"
+            onClick={() => {
+              setOpen((prev) => !prev)
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="border p-2 hover:bg-gray-50"
+            onClick={() => {
+              setOpen((prev) => !prev)
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 

c32e834c48b3cd97768638e52c03a2f45c92b15c
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-06-22 16:11:30 +0530

    ..

--- src/redux/slices/invoiceSlice.js
+++ src/redux/slices/invoiceSlice.js
@@ -4,7 +4,7 @@ import getCookieValue from "../../utils/getCookieValue";
 import axios from "axios";
 axios.defaults.withCredentials = true
 
-const createInvoice = createAsyncThunk("invoice/create", async (body) => {
+const createInvoice = createAsyncThunk("invoice/createInvoice", async (body) => {
     try {
       const res = await axios.post(
         `http://localhost:4598/api/v1/auth/login`,
@@ -16,5 +16,9 @@ const createInvoice = createAsyncThunk("invoice/create", async (body) => {
       return rejectWithValue(err);
     }
   });
+
+  const deleteInvoice=createAsyncThunk("invoice/deleteInvoice",async(id)=>{
+    
+  } 
 
 // export {}

0ef4e48e1a0366b77e2d06c73d436217fbb9cb2b
Ishan <Braviaspan2002@gmail.com>
2024-06-23 15:03:41 +0530

    Completed template

--- src/components/Invoice/InvoicePreview.jsx
+++ src/components/Invoice/InvoicePreview.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { getProfile } from "../../redux/slices/userSlice"
+import numWords from "num-words"
 
 const InvoicePreview = forwardRef((props, ref) => {
   const dispatch = useDispatch()
@@ -20,7 +21,7 @@ const InvoicePreview = forwardRef((props, ref) => {
   const products = [
     {
       name: "Product 1 ",
-      hsn_code: "123456",
+      hsn_code: "1234",
       price: 1000,
       quantity: 1,
       discount: {
@@ -30,7 +31,7 @@ const InvoicePreview = forwardRef((props, ref) => {
     },
     {
       name: "Product 2",
-      hsn_code: "123456",
+      hsn_code: "1234",
       price: 1500,
       quantity: 1,
       discount: {
@@ -44,11 +45,14 @@ const InvoicePreview = forwardRef((props, ref) => {
     <div ref={ref} className="min-h-full bg-white p-2">
       {/* Organisation Details */}
       <div className="flex justify-between border border-b-0 border-black p-1">
-        <div className="text-xs">
+        <div className="text-sm">
           <p className="text-2xl font-black uppercase">{user?.org?.name}</p>
-          <p className="">5832/33,Plot - 3A/UA, 2nd Floor, Jawahar Nagar</p>
-          <p>New Delhi - 110007, Delhi, India</p>
-          <p>Phone: 9350232008</p>
+          <p className="">{user?.org?.address?.streetAddress}</p>
+          <p>
+            {user?.org?.address?.city} - {user?.org?.address?.zip},{" "}
+            {user?.org?.address?.state}, {user?.org?.address?.country}
+          </p>
+          <p>Phone: {user?.phone}</p>
           <p>Email: {user?.org?.email}</p>
           <p>GSTIN: {user?.org?.gstin}</p>
         </div>
@@ -78,21 +82,21 @@ const InvoicePreview = forwardRef((props, ref) => {
       {/* Customer Deatils */}
       <div className="border border-b-0 border-black text-sm">
         <div className="border-b border-black p-1">
-          <p className="font-bold">Issued to</p>
+          <p className="text-lg font-bold">Issued to</p>
           <div className="flex justify-between">
-            <p className="text-base">Abhinav Infrabuild Pvt Ltd</p>
+            <p className="">Abhinav Infrabuild Pvt Ltd</p>
             <p>GSTIN: 23AAHCA9425D1ZY</p>
           </div>
         </div>
         <div className="flex gap-2">
           <div className="w-1/2 border-r border-black p-1">
-            <p className="font-bold">Billing Address</p>
+            <p className="text-lg font-bold">Billing Address</p>
             <p>207-208, Industry House, AB Road</p>
             <p>Indore, ZIP: 452018</p>
             <p>Madhya Pradesh, India</p>
           </div>
           <div className="p-1">
-            <p className="font-bold">Shipping Address</p>
+            <p className="text-lg font-bold">Shipping Address</p>
             <p>207-208, Industry House, AB Road</p>
             <p>Indore, ZIP: 452018</p>
             <p>Madhya Pradesh, India</p>
@@ -101,60 +105,144 @@ const InvoicePreview = forwardRef((props, ref) => {
       </div>
 
       {/* Table */}
-      <div className="grid grid-cols-10 border border-b-0 border-r-0 border-black text-base font-bold">
-        <div className=" border-r border-black bg-primary p-2 text-white">
+      <div className="flex w-full border border-b-0 border-r-0 border-black text-base font-bold">
+        <div className="w-[8%] border-r border-black bg-primary p-2 text-white">
           S.No
         </div>
-        <div className="col-span-3 border-b border-r border-black bg-primary p-2 text-white">
+        <div className="col-span-3 w-[27%] border-b border-r border-black bg-primary p-2 text-white">
           Item
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
+        <div className="w-[8%] border-r border-black bg-primary p-2 text-white">
           HSN <br /> Code
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
+        <div className="w-[8%] border-r border-black bg-primary p-2 text-white">
           Qty
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
-          Price
+        <div className="w-[13%] border-r border-black bg-primary p-2 text-white">
+          Price <br /> (INR)
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
-          Discount
+        <div className="w-[8%] border-r border-black bg-primary p-2 text-white">
+          Disc.
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
-          Final <br /> Price
+        <div className="w-[13%] border-r border-black bg-primary p-2 text-white">
+          Final Price <br /> (INR)
         </div>
-        <div className=" border-r border-black bg-primary p-2 text-white">
+        <div className="w-[15%] border-r border-black bg-primary p-2 text-white">
           Amount <br /> (INR)
         </div>
       </div>
       {/* <div className="bg-primary p-1 text-white">Amount <br /> (INR)</div> */}
       {products.map((product, index) => {
         return (
-          <div key={index} className="grid grid-cols-10 border border-b-0 border-r-0 border-black text-sm">
-            <div className="border-r border-black p-2">{index + 1}</div>
-            <div className="col-span-3 border-r border-black p-2">
+          <div
+            key={index}
+            className="flex border border-b-0 border-r-0 border-black text-sm"
+          >
+            <div className="w-[8%] border-r border-black p-2">{index + 1}</div>
+            <div className="w-[27%] border-r border-black p-2">
               {product.name}
             </div>
-            <div className="border-r border-black p-2">{product.hsn_code}</div>
-            <div className="border-r border-black p-2">{product.quantity}</div>
-            <div className="border-r border-black p-2">{product.price}</div>
-            <div className="border-r border-black p-2">
+            <div className="w-[8%] border-r border-black p-2">
+              {product.hsn_code}
+            </div>
+            <div className="w-[8%] border-r border-black p-2">
+              {product.quantity}
+            </div>
+            <div className="w-[13%] border-r border-black p-2">
+              {product.price}
+            </div>
+            <div className="w-[8%] border-r border-black p-2">
               {product.discount.value}
             </div>
-            <div className="border-r border-black p-2">
+            <div className="w-[13%] border-r border-black p-2">
               {product.price - product.discount.value}
             </div>
-            <div className="border-r border-black p-2">
+            <div className="col-span-2 w-[15%] border-r border-black p-2">
               {product.price * product.quantity}
             </div>
           </div>
         )
       })}
-      <div className="grid grid-cols-10 border border-b-0 border-r-0 border-black text-sm">
-        <div className="col-span-9 items-end border-b border-r border-black p-1 text-right">
+      <div className="flex border border-b-0 border-r-0 border-black text-sm">
+        <div className="w-[85%] border-b border-r border-black p-2 text-right font-bold">
           Sub Total
         </div>
-        <div className="border-b border-r border-black p-1"> 2500</div>
+        <div className="w-[15%] border-b border-r border-black p-2"> 2500</div>
+      </div>
+
+      <div className="flex text-sm">
+        <div className="w-[64%]">
+          <div className="border border-t-0 border-black p-1">
+            <p className="text-lg font-bold">Amount in Words</p>
+            <p className="capitalize">{numWords(3876)}</p>
+          </div>
+          <div className="border border-t-0 border-black p-1">
+            <p className="text-lg font-bold">Bank Details</p>
+            <div className="flex w-full">
+              <div className="flex w-[40%]">
+                <p className="w-14 font-bold">Bank</p>
+                <p>{user?.banking?.bankName}</p>
+              </div>
+
+              <div className="flex w-1/2">
+                <p className="w-32 font-bold">Account Number</p>
+                <p>{user?.banking?.accountNumber}</p>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="flex w-[40%]">
+                <p className="w-14 font-bold">Branch</p>
+                <p>{user?.banking?.branch}</p>
+              </div>
+              <div className="flex">
+                <p className="w-32 font-bold">IFSC Code</p>
+                <p>{user?.banking?.ifsc}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Taxes */}
+        <div className="w-[36%] p-1 border-r border-b border-black">
+          <div className="flex w-full justify-between">
+            <p className="px-2 text-right w-[35%]">IGST</p>
+            <p className="px-2 w-[25%]">10%</p>
+            <p className="w-[40%] px-2">230</p>
+          </div>
+          <div className="flex w-full justify-between">
+            <p className="px-2 text-right w-[35%]">CGST</p>
+            <p className="px-2 w-[25%]">10%</p>
+            <p className="w-[40%] px-2">230</p>
+          </div>
+          <div className="flex w-full justify-between">
+            <p className="px-2 text-right w-[35%]">SGST</p>
+            <p className="px-2 w-[25%]">10%</p>
+            <p className="w-[40%] px-2">230</p>
+          </div>
+          <div className="flex w-full justify-between">
+            <p className="px-2 text-right w-[35%]">Round off</p>
+            <p className="w-[40%] px-2">230</p>
+          </div>
+          <div className="flex w-full justify-between bg-primary text-white">
+            <p className="p-2 text-right w-[30%] text-lg">Total</p>
+            <p className=" p-2 text-xl font-bold">Rs. 23054.00</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Terms and Conditions */}
+      <div className="flex">
+        <div className="p-1 border border-t-0 border-black w-[64%]">
+          <div className="flex justify-between items-center">
+          <p className="font-bold text-lg">Terms and Conditions</p>
+          <span className="text-sm">E & O.E</span>
+          </div>
+        </div>
+        <div className="p-1 border-r border-b border-black w-[36%] flex flex-col items-end">
+          <p>For {user?.org?.name}</p>
+          <div className="h-28"></div>
+          <p>Authorized Signature</p>
+        </div>
       </div>
     </div>
   )

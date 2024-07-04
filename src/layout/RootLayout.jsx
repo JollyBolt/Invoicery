c48ebeea078fdf588a4777376826d24cc8e4d09a
Ishan <Braviaspan2002@gmail.com>
2024-07-05 01:38:31 +0530

    minor fixes

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,12 +2,10 @@ import React from "react"
 import { useEffect } from "react"
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
-import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { authSlice } from "../redux/slices/authSlice"
 
 const RootLayout = () => {
-  const [open, setOpen] = useState(false)
   const { refreshAuth } = authSlice.actions
   const dispatch = useDispatch()
   useEffect(() => {
@@ -15,15 +13,10 @@ const RootLayout = () => {
   }, [])
   return (
     <>
-      <Sidebar
-        open={open}
-        setOpen={setOpen}
-        navBg="foreground"
-        navHeading="secondary"
-      />
+      <Sidebar navBg="foreground" navHeading="secondary" />
       <div
         id="parent"
-        className={`ml-[64px] bg-slate-100 transition-all duration-500`}
+        className={`ml-[56px] bg-slate-100 transition-all duration-500`}
       >
         <Outlet />
       </div>

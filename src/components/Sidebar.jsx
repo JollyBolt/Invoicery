653297be05e5d33eecca0f017e0685edff17c735
Ishan <Braviaspan2002@gmail.com>
2024-07-05 00:49:14 +0530

    Added active links and some ui changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,16 +4,17 @@ import { HiMenuAlt2 } from "../assets/index"
 import { navLinks } from "../constants"
 import { useNavigate } from "react-router-dom"
 import { IoPowerOutline } from "react-icons/io5"
-import data from "../demoData.json"
 import { useDispatch, useSelector } from "react-redux"
 import { getProfile } from "../redux/slices/userSlice"
 
-function Sidebar({ open, setOpen, navBg, navHeading }) {
-  const location = useLocation()
+function Sidebar({ navBg, navHeading }) {
+  const [open, setOpen] = useState(false)
+
+  const { pathname } = useLocation()
   const navigate = useNavigate()
 
   const dispatch = useDispatch()
-  const { user,loading } = useSelector((state) => state.user)
+  const { user, loading } = useSelector((state) => state.user)
   const { loggedIn } = useSelector((state) => state.auth)
 
   useEffect(() => {
@@ -26,40 +27,50 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
     getUserData()
   }, [loggedIn])
 
+  const checkActive = (href) => {
+    const n = href.length
+    // Check if the pathname starts with the link's href
+    if (pathname.startsWith(href)) {
+      // If the pathname is longer than the link's href and the character after the link's href is a '/', then the link is considered active.
+      if (pathname.length > n && pathname[n] === "/") return true
+
+      // If the pathname is exactly the same length as the link's href, then the link is considered active.
+      if (pathname.length == n) return true
+    }
+    return false
+  }
+
   return (
     <div>
       <nav id="nav" className="fixed top-0 z-10">
         <div
           style={{ backgroundColor: `var(--${navBg})` }}
-          className={`flex h-screen flex-col justify-between pb-5 shadow-lg shadow-slate-300 ${
-            open ? "w-[210px]" : "w-[64px]"
-          } text-black transition-all delay-[25ms] duration-500 ${
-            open ? "pl-4 pr-2" : "px-4"
+          className={`flex h-screen flex-col justify-between p-2 text-black shadow-lg shadow-slate-300 transition-all delay-[25ms] duration-500 ${
+            open ? "w-[230px]" : "w-[56px]"
           }`}
+          onMouseEnter={() => setOpen(true)}
+          onMouseLeave={() => setOpen(false)}
         >
           <div>
-            <div className={`flex py-3 pl-[5px]`}>
+            {/* <div className={`flex py-3`}>
               <HiMenuAlt2
                 size={24}
                 className="cursor-pointer"
                 onClick={() => setOpen(!open)}
               />
-            </div>
+            </div> */}
             <div className="relative mt-4 flex flex-col justify-between gap-4">
-              <div className="overflow-hidden text-nowrap">
-                {/* <img
-                  src={"/logo.png"}
-                  width={40}
-                  height={40}
-                  alt="Logo"
-                  className="inline"
-                /> */}
+              <div className="flex gap-3.5 overflow-hidden text-nowrap px-2">
+                <div className="flex w-6 shrink-0 justify-center border border-black text-2xl font-extrabold uppercase text-primary">
+                  {" "}
+                  I{" "}
+                </div>
                 <h2
                   style={{
                     transitionDelay: `${2}00ms`,
                     color: `var(--${navHeading})`,
                   }}
-                  className={`inline whitespace-pre text-xl font-black uppercase text-transparent duration-500 ${
+                  className={`inline whitespace-pre text-2xl font-black uppercase text-transparent duration-500 ${
                     !open && "translate-x-28 overflow-hidden opacity-0"
                   }`}
                 >
@@ -68,38 +79,37 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
               </div>
 
               <div className="flex flex-col justify-between gap-y-4 overflow-hidden">
-                {navLinks?.map((menu, i) => (
-                  <Link
-                    to={menu?.link}
-                    key={i}
-                    className={` ${
-                      menu?.margin && "mt-5"
-                    } group flex items-center gap-3.5 rounded-md py-2 pl-1 text-sm font-medium text-black hover:bg-primary hover:text-white`}
-                  >
-                    <div>{React.createElement(menu?.icon, { size: "24" })}</div>
-                    <h2
-                      className={`text-md overflow-hidden whitespace-pre text-black [transition:transform_.3s_cubic-bezier(0.4,0,0.2,1),color_0s] group-hover:text-white ${
-                        open ? "translate-x-0 opacity-100" : "translate-x-24"
-                      }`}
+                {navLinks?.map((menu, i) => {
+                  const isActive = checkActive(menu.link)
+                  return (
+                    <Link
+                      to={menu?.link}
+                      key={i}
+                      className={` ${
+                        isActive
+                          ? "bg-primary text-white"
+                          : "text-slate-500 hover:text-black"
+                      } group flex items-center gap-3.5 rounded-md p-2 text-sm font-medium`}
                     >
-                      {menu?.name}
-                    </h2>
-
-                    <h2
-                      className={`${
-                        open && "hidden"
-                      } absolute left-20 w-0 overflow-hidden whitespace-pre rounded-md bg-white px-0 py-0 font-semibold text-gray-900 drop-shadow-lg group-hover:left-14 group-hover:w-fit group-hover:px-2 group-hover:py-1 group-hover:duration-300`}
-                    >
-                      {menu?.name}
-                    </h2>
-                  </Link>
-                ))}
+                      <div>
+                        <menu.icon size={24} />
+                      </div>
+                      <h2
+                        className={`text-md overflow-hidden whitespace-pre [transition:transform_.3s_cubic-bezier(0.4,0,0.2,1),color_0s] ${
+                          open ? "translate-x-0 opacity-100" : "translate-x-24"
+                        }`}
+                      >
+                        {menu?.name}
+                      </h2>
+                    </Link>
+                  )
+                })}
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-y-4 overflow-hidden border-t border-gray-500 pt-2">
-            <button
+          <div className="flex flex-col gap-4 overflow-hidden border-t border-gray-500 px-2 pt-2">
+            <div
               onClick={() => {
                 // localStorage.removeItem("loggedIn");
                 document.cookie =
@@ -109,9 +119,9 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
               }}
               className={`text-red-500 transition-[border-radius] hover:bg-red-500 hover:text-white ${
                 open ? "rounded-rounded delay-0" : "rounded-[50%] delay-300"
-              } duration-400 group flex items-center gap-3.5 py-1 pl-1 text-sm font-medium ease-linear`}
+              } duration-400 flex items-center gap-3.5 py-1 ease-linear`}
             >
-              <div>{React.createElement(IoPowerOutline, { size: "24" })}</div>
+              <IoPowerOutline size={24} className="shrink-0" />
               <h2
                 className={`text-md overflow-hidden whitespace-pre [transition:transform_.3s_cubic-bezier(0.4,0,0.2,1),color_0s] ${
                   open ? "translate-x-0 opacity-100" : "translate-x-24"
@@ -119,40 +129,20 @@ function Sidebar({ open, setOpen, navBg, navHeading }) {
               >
                 Logout
               </h2>
-            </button>
+            </div>
 
-            {/* <div className="flex  overflow-hidden ">
-              <div className="flex items-center py-[1px] w-full text-black  rounded-rounded ">
-                <div>
-                  {data.name.split(" ").map(function (word, i) {
+            <div
+              className={`group flex items-center gap-3.5 rounded-md py-2 text-sm font-medium text-black`}
+            >
+              <div className="flex flex-row flex-nowrap">
+                {user.name &&
+                  user.name.split(" ").map(function (word, i) {
                     return (
                       <span key={i} className="inline-block text-lg">
                         {word[0]}
                       </span>
-                    );
+                    )
                   })}
-                </div>
-              </div>
-              <div className={`flex flex-col flex-nowrap [transition:transform_.3s_cubic-bezier(0.4,0,0.2,1),color_0s]  overflow-hidden  ${
-                  open? "opacity-100 translate-x-0  ":"translate-x-24"
-                }`} >
-                <h3 className="text-lg leading-none font-semibold ">
-                  {data.name}
-                </h3>
-                <p className="text-xs ">{data.email}</p>
-              </div>
-            </div> */}
-            <div
-              className={`group flex items-center gap-3.5 rounded-md py-2 pl-1 text-sm font-medium text-black`}
-            >
-              <div className="flex flex-row flex-nowrap">
-                {user.name && user.name.split(" ").map(function (word, i) {
-                  return (
-                    <span key={i} className="inline-block text-lg">
-                      {word[0]}
-                    </span>
-                  )
-                })}
               </div>
               <div
                 className={`text-blac text-md flex flex-col overflow-hidden whitespace-pre [transition:transform_.3s_cubic-bezier(0.4,0,0.2,1),color_0s] ${

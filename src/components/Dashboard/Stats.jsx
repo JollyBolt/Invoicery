f65a5e674f0c88664e19f46b0fb6c1c017a18fd6
Ishan <Braviaspan2002@gmail.com>
2024-07-10 01:51:55 +0530

    Updated code for revenue

--- src/components/Dashboard/Stats.jsx
+++ src/components/Dashboard/Stats.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Stats = ({ title, number, icon }) => {
+const Stats = ({ title = "", number = 0, icon }) => {
   return (
     <div className="col-span-2 flex flex-col rounded-rounded bg-white p-5 py-8">
       <div className="flex items-center justify-between text-base">
@@ -8,10 +8,10 @@ const Stats = ({ title, number, icon }) => {
         <p className="font-numbers text-2xl text-primary">{icon}</p>
       </div>
       <span className="flex h-full items-center font-numbers text-6xl font-semibold text-primary">
-        {number}
+        {title == "Total Revenue" ? number.toLocaleString() : number}
       </span>
     </div>
-  );
-};
+  )
+}
 
 export default Stats;

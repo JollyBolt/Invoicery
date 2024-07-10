7efd95f5b1db318bca637c91b4d4402e36b97a9c
Ishan <Braviaspan2002@gmail.com>
2024-07-11 01:50:42 +0530

    Added view invoice modal and functions to other buttons

--- src/components/Table/InvoiceActionsDropdown.jsx
+++ src/components/Table/InvoiceActionsDropdown.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useRef, useState } from "react"
 import { BsThreeDotsVertical } from "../../assets"
 import { useDispatch } from "react-redux"
 import { deleteInvoice } from "../../redux/slices/invoiceSlice"
+import { useNavigate } from "react-router-dom"
+import ViewInvoice from "../Invoice/ViewInvoice"
 
 const InvoiceActionsDropdown = ({ row }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false) //State to manage the dropdown
+  const [modalOpen, setModalOpen] = useState(false) // This is to manage the edit modal
+
+  const navigate = useNavigate()
   const ref = useRef()
 
   const dispatch = useDispatch()
@@ -26,46 +31,54 @@ const InvoiceActionsDropdown = ({ row }) => {
     }
   }, [])
   return (
-    <div
-      className="relative cursor-pointer rounded-rounded"
-      onClick={() => setOpen((prev) => !prev)}
-      ref={ref}
-    >
-      <BsThreeDotsVertical />
+    <>
+      <ViewInvoice
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        invoiceState={row.original}
+      />
       <div
-        className={`absolute bottom-0 left-4 ${!open && "scale-0"} flex w-[80px] origin-bottom-left flex-col bg-white transition-all`}
+        className="relative cursor-pointer rounded-rounded"
+        onClick={() => setOpen((prev) => !prev)}
+        ref={ref}
       >
-        <button
-          className="border p-2 hover:bg-gray-50"
-          onClick={(e) => {
-            e.stopPropagation()
-            setOpen((prev) => !prev)
-          }}
+        <BsThreeDotsVertical />
+        <div
+          className={`absolute bottom-0 left-4 ${!open && "scale-0"} flex w-[80px] origin-bottom-left flex-col bg-white transition-all`}
         >
-          View
-        </button>
-        <button
-          className="border p-2 hover:bg-gray-50"
-          onClick={(e) => {
-            e.stopPropagation()
-            setOpen((prev) => !prev)
-          }}
-        >
-          Edit
-        </button>
-        <button
-          className="border p-2 hover:bg-gray-50"
-          onClick={(e) => {
-            e.stopPropagation()
-            dispatch(deleteInvoice(row.original._id))
-            location.reload()
-            setOpen((prev) => !prev)
-          }}
-        >
-          Delete
-        </button>
+          <button
+            className="border p-2 hover:bg-gray-50"
+            onClick={(e) => {
+              e.stopPropagation()
+              setOpen((prev) => !prev)
+              setModalOpen((prev) => !prev)
+            }}
+          >
+            View
+          </button>
+          <button
+            className="border p-2 hover:bg-gray-50"
+            onClick={(e) => {
+              e.stopPropagation()
+              setOpen((prev) => !prev)
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="border p-2 hover:bg-gray-50"
+            onClick={(e) => {
+              e.stopPropagation()
+              dispatch(deleteInvoice(row.original._id))
+              location.reload()
+              setOpen((prev) => !prev)
+            }}
+          >
+            Delete
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 

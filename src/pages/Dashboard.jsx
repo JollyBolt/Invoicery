9e59fd132c19ea54c02290a438a085f97a6c61ea
Ishan <Braviaspan2002@gmail.com>
2024-07-13 12:43:20 +0530

    Added yearly data from backend

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import PageWrapper from "../hoc/PageWrapper";
-import BarChart from "../components/Charts/BarChart";
-import LineChart from "../components/Charts/LineChart";
+import React, { useState, useEffect } from "react"
+import PageWrapper from "../hoc/PageWrapper"
+import BarChart from "../components/Charts/BarChart"
+import LineChart from "../components/Charts/LineChart"
 import {
   FaAngleLeft,
   FaAngleRight,
@@ -9,7 +9,7 @@ import {
   FaFileInvoiceDollar,
   BsPeopleFill,
   BsBoxSeamFill,
-} from "../assets/index";
+} from "../assets/index"
 import Stats from "../components/Dashboard/Stats"
 import { useSelector, useDispatch } from "react-redux"
 import Auth from "../components/Auth"
@@ -17,7 +17,12 @@ import axios from "axios"
 import getCookieValue from "../utils/getCookieValue"
 
 const Dashboard = () => {
-  const [currentYear, setCurrentYear] = useState(2024)
+  const [currentYear, setCurrentYear] = useState(
+    new Date().getFullYear().toString(),
+  )
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
+
+  const [revenue, setRevenue] = useState(null) // This is to manage the revenue
 
   const [stats, setStats] = useState(null)
 
@@ -32,10 +37,29 @@ const Dashboard = () => {
     )
     setStats(data.stats)
   }
-
   useEffect(() => {
     getStats()
   }, [])
+
+  const getDashboardChartData = async () => {
+    const { data } = await axios.get(
+      "http://localhost:4598/api/v1/invoice/getdashboardchartdata",
+      {
+        headers: {
+          Authorization: "Bearer " + getCookieValue("authToken"),
+        },
+        params: {
+          year: currentYear,
+          month: currentMonth,
+        },
+      },
+    )
+    setRevenue(data)
+  }
+
+  useEffect(() => {
+    getDashboardChartData()
+  }, [currentYear, currentMonth])
 
   const monthNames = [
     "January",
@@ -51,8 +75,6 @@ const Dashboard = () => {
     "November",
     "December",
   ]
-
-  const [currentmonth, setCurrentMonth] = useState(new Date().getMonth())
 
   const [chart, setChart] = useState("line")
   const handleChange = (e) => {
@@ -85,11 +107,11 @@ const Dashboard = () => {
           {
             //Yearly Chart
           }
-          <div className="col-span-4 row-span-2 rounded-rounded bg-background p-5">
+          <div className="bg-background col-span-4 row-span-2 rounded-rounded p-5">
             <div className="mb-3 flex items-center justify-between">
               <div className="flex items-center gap-3 text-xl">
                 <button onClick={() => setCurrentYear((prev) => prev - 1)}>
-                  <FaAngleLeft className="text-primary"/>
+                  <FaAngleLeft className="text-primary" />
                 </button>
                 <span className="font-numbers font-bold uppercase text-foreground">
                   <span className="font-numbers">{currentYear}</span> Revenue
@@ -102,17 +124,25 @@ const Dashboard = () => {
                 id="chartType"
                 value={chart}
                 onChange={handleChange}
-                className="mt-1 block w-[13%] rounded-md border bg-background text-foreground border-gray-300 px-2 py-2 text-base focus:outline-none sm:text-sm"
+                className="bg-background mt-1 block w-[13%] rounded-md border border-gray-300 px-2 py-2 text-base text-foreground focus:outline-none sm:text-sm"
               >
                 <option value="bar">Bar</option>
                 <option value="line">Line</option>
               </select>
             </div>
-            {chart == "line" ? (
-              <LineChart currentYear={currentYear} />
-            ) : (
-              <BarChart currentYear={currentYear} />
-            )}
+            <>
+              {chart == "line" ? (
+                <LineChart
+                  currentYear={currentYear}
+                  revenue={revenue?.revenueForYearlyChart}
+                />
+              ) : (
+                <BarChart
+                  currentYear={currentYear}
+                  revenue={revenue?.revenueForYearlyChart}
+                />
+              )}
+            </>
           </div>
 
           <Stats
@@ -129,21 +159,21 @@ const Dashboard = () => {
           {
             //Recent Invoices Table
           }
-          <div className="col-span-4 rounded-rounded bg-background text-foreground p-5">
+          <div className="bg-background col-span-4 rounded-rounded p-5 text-foreground">
             {" "}
             Recent Invoices
           </div>
 
           {}
-          <div className="col-span-4 rounded-rounded bg-background p-5">
+          <div className="bg-background col-span-4 rounded-rounded p-5">
             <div>
-              <div className="flex items-center gap-3 text-xl ">
+              <div className="flex items-center gap-3 text-xl">
                 <button
                   onClick={() =>
                     setCurrentMonth((prev) => (prev > 0 ? prev - 1 : 11))
                   }
                 >
-                  <FaAngleLeft  className="text-primary"/>
+                  <FaAngleLeft className="text-primary" />
                 </button>
                 <span className="font-bold uppercase text-foreground">
                   {monthNames[currentmonth]}
@@ -164,4 +194,4 @@ const Dashboard = () => {
   )
 }
 
-export default PageWrapper(Dashboard, "dashboard");
+export default PageWrapper(Dashboard, "dashboard")

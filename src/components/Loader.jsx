c88453d7b39cf848e4cacebbae4e1f24d863482c
Ishan <Braviaspan2002@gmail.com>
2024-07-20 23:11:06 +0530

    Updated loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Loader1 } from "../assets";
-function Loader() {
+function Loader({ height = "600px" }) {
   return (
-    <div className={`w-full flex justify-center h-[600px] items-center `}>
-      <img
-        src={Loader1}
-        alt="loader"
-        className="w-24"
-      />
+    <div
+      className={`flex w-full items-center justify-center`}
+      style={{ height: height }}
+    >
+      <img src={Loader1} alt="loader" className="w-24" />
     </div>
-  );
+  )
 }
 
 export default Loader;

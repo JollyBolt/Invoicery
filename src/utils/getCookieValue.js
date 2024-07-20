384474674b733b3683a9773dad1f562597cf724c
Ishan <Braviaspan2002@gmail.com>
2024-07-20 17:56:52 +0530

    removed console log

--- src/utils/getCookieValue.js
+++ src/utils/getCookieValue.js
@@ -1,10 +1,9 @@
 export default function getCookieValue(name) {
   function escape(s) {
-    return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1");
+    return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1")
   }
   var match = document.cookie.match(
     RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)"),
-  );
-  console.log(document.cookie)
-  return match ? match[1] : null;
+  )
+  return match ? match[1] : null
 }

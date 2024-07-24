7d6f3cd6ef444000f9c145448cbabbd8697e1804
Ishan <Braviaspan2002@gmail.com>
2024-07-25 03:00:55 +0530

    Added simple template

---
+++ src/components/Invoice/MultiStepForm/Simple/SimpleTemplate.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const SimpleTemplate = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center bg-background p-2 text-foreground">
+      <div className="text-9xl font-black uppercase">
+        Coming
+        <br /> Soon
+      </div>
+    </div>
+  )
+}
+
+export default SimpleTemplate
